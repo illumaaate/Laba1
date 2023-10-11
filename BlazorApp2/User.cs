597f8c827342111c54eb1aacf5e5538b4af7788b
artem <artem@ARTEMPC>
2023-10-12 00:59:26 +0300

    Добавьте файлы проекта.

---
+++ BlazorApp2/User.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorApp2
+{
+    public class User
+    {
+        public string SName { get; set; }
+        public string Name { get; set; }
+        public string Otch { get; set; }
+        public int? Age { get; set; }
+        public string Resume
+        {
+            get
+            {
+                return SName + " " + Name + " " + Otch + ". Полных лет: " + Age;
+            }
+        }
+        public User(string sname, string name, string otch, int? age)
+        {
+            SName = sname;
+            Name = name;
+            Otch = otch;
+            Age = age;
+        }
+    }
+}
